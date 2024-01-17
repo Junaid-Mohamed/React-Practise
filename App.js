6d527687cc4000168d8cc1a90c8922075fc44773
Junaid-Mohamed <junaid7mohamed@gmail.com>
2024-01-17 19:31:13 +0530

    creating nesting elements  using plain React

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-const heading = React.createElement("h1",{id:"heading"},`Hi World from React App`);
+
+// creating nested element.
+/**
+ * <div id="parent">
+ *  <div id="child1">
+ *      <h1>This is h1 tag inside child div</h1>
+ *      <h2>This is h2 tag inside child div and syblling of h1 of child 1</h2>
+ *  </div>
+ * <div id="child2">
+ *      <h1>This is h1 tag inside child2 div</h1>
+ *      <h2>This is h2 tag inside child2 div and syblling of h1 of child 2</h2>
+ *  </div>
+ * </div>
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},
+[React.createElement("h1",{},"This is h1 tag inside child1 div"),React.createElement("h2",{},"This is h2 tag inside child div and syblling of h1 of child 1")]),React.createElement("div",{id:"child2"},
+[React.createElement("h1",{},"This is h1 tag inside child2 div"),React.createElement("h2",{},"This is h2 tag inside child div and syblling of h1 of child 2")])]
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
 // {} this object is to define attributes of that tag.
 
-console.log(heading); // this is the React object, the render method is responsible to 
+console.log(parent); // this is the React object, the render method is responsible to 
 // convert the React object into the h1 tag.
