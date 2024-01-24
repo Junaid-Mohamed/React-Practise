50613c62ab97762fd42a34941f300d915fb239be
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-01-24 23:06:09 +0530

    added header coomponent for building food app:

--- App.js
+++ App.js
@@ -2,22 +2,37 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
-const Title = () =>(
-<h1 className="head" >React using JSX</h1>
-);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-
-// ?React Element
 
 
 // React Functional Component
 
-const HeadingComponent = () => <h1 className="heading" >React Functional Component</h1>
+const AppLayout = () => {
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// this only react elements can be rendered.
-// root.render(heading);
 
-// for functional component you render like thhis
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
